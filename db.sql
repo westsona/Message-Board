a9f7940ebc025a63a4102aa6d30f6a3a031cf5bb
West Liu <a36391771@126.com>
2020-03-05 11:29:05 +0800

    修改db

--- db.sql
+++ db.sql
@@ -1,15 +1,43 @@
+/*
+ Navicat Premium Data Transfer
 
- use gbook;
+ Source Schema         : message
 
-create table admin(
-    username varchar(20) not null,
-    userpass varchar(20) not null
-);
+ Target Server Type    : MySQL
+ Target Server Version : 100411
+ File Encoding         : 65001
 
-create table message(
-    id int(4) not null auto_increment primary key,
-    author varchar(20) not null,
-    addtime datetime not null,
-    content varchar(1000) not null,
-    reply varchar(1000) not null
-);
+ Date: 05/03/2020 11:24:20
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
+  `id` int(11) NOT NULL,
+  `article_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `time` timestamp(0) NULL DEFAULT current_timestamp(),
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `is_praised` tinyint(255) NULL DEFAULT 0,
+  `is_toped` tinyint(255) NULL DEFAULT 0,
+  `is_reported` tinyint(255) NULL DEFAULT 0,
+  `article_id` int(11) NOT NULL,
+  `order_index` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
